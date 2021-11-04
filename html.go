64dcdc49249ba740335e5ea326a1ed18e7c24d90
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-11-04 09:20:50 -0300

    WebScrapper Go Lang

---
+++ html.go
@@ -0,0 +1,24 @@
+package html
+
+import (
+	"io/ioutil"
+	"net/http"
+	"regexp"
+)
+
+// Titles is titles
+func Titles(urls ...string) <-chan string {
+	channel := make(chan string)
+
+	for _, url := range urls {
+		go func(url string) {
+			resp, _ := http.Get(url)
+			html, _ := ioutil.ReadAll(resp.Body)
+
+			r, _ := regexp.Compile(`<title>(.*?)<\/title>`)
+			channel <- r.FindStringSubmatch(string(html))[1]
+		}(url)
+	}
+
+	return channel
+}
